d34526c6f77881527bf30d7fecd0d831eef66de4
rajan2023 <90898020+rajan2023@users.noreply.github.com>
2024-09-19 16:27:45 +0545

    fix: reaction transition

--- src/pages/post-engagements/page/post-engagement-edit/components/tabs/post-tab/tab-1/select-a-post-tab.tsx
+++ src/pages/post-engagements/page/post-engagement-edit/components/tabs/post-tab/tab-1/select-a-post-tab.tsx
@@ -2,9 +2,10 @@ import { widgetConstant } from "./constant/widget.constant";
 
 export default function SelectAPost() {
   return (
-    <div className="flex flex-col gap-2 overflow-hidden mt-10">
+    <div className="flex flex-col gap-2 overflow-hidden mt-4">
+      <p className="flex place-content-center">Select A Post</p>
       <div
-        className="flex flex-row gap-4 flex-wrap w-max mx-auto"
+        className="flex flex-row gap-4 flex-wrap w-max mx-auto mt-10"
         dangerouslySetInnerHTML={{ __html: widgetConstant }}
       ></div>
     </div>
